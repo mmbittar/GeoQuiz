1a625f0f085223f7a3609f4b06d028ac89784276
Marcelo Bittar <bittar@rocketmail.com>
2018-02-09 15:41:38 -0500

    Initial commit

---
+++ app/src/main/java/assignment1/bittar/geoquiz/Question.java
@@ -0,0 +1,31 @@
+package assignment1.bittar.geoquiz;
+
+/**
+ * Created by mmnit on 2/9/2018.
+ */
+
+public class Question {
+    private int mQuestionResId;
+    private boolean mQuestionAnswerTrue;
+
+    public Question(int questionResId, boolean questionAnswerTrue) {
+        mQuestionResId = questionResId;
+        mQuestionAnswerTrue = questionAnswerTrue;
+    }
+
+    public int getQuestionResId() {
+        return mQuestionResId;
+    }
+
+    public void setQuestionResId(int questionResId) {
+        mQuestionResId = questionResId;
+    }
+
+    public boolean isQuestionAnswerTrue() {
+        return mQuestionAnswerTrue;
+    }
+
+    public void setQuestionAnswerTrue(boolean questionAnswerTrue) {
+        mQuestionAnswerTrue = questionAnswerTrue;
+    }
+}
