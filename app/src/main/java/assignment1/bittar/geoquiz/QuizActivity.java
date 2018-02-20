b6e49dad960b9410edb90e8cd6c25151e0156604
Marcelo Bittar <bittar@rocketmail.com>
2018-02-19 21:28:01 -0500

    Added landscape layout and comments

--- app/src/main/java/assignment1/bittar/geoquiz/QuizActivity.java
+++ app/src/main/java/assignment1/bittar/geoquiz/QuizActivity.java
@@ -21,8 +21,13 @@ public class QuizActivity extends AppCompatActivity {
     private ImageButton mNextButton;
     private ImageButton mBackButton;
     private TextView mQuestionText;
+    private TextView mQuestAns;
+
 
     private static final String KEY_INDEX = "index";
+    private static final String TOTAL_ANSWERED = "answered";
+    private static final String CORRECT_ANSWERS = "correct";
+
 
     /**
      *  Array of Question objects containing all questions and answers of our quiz game
@@ -38,11 +43,23 @@ public class QuizActivity extends AppCompatActivity {
 
     //Current index of our Questions array
     private int mCurrentIndex = 0;
-
+    //Current number of correct answers. Used to calculate grade
+    private int mCorrectAnswers = 0;
+    //Total questions answered. Used to calculate grade
+    private int mTotalAnswered = 0;
     @Override
     public void onSaveInstanceState(Bundle savedInstanceState){
         super.onSaveInstanceState(savedInstanceState);
+        //Saves current question index
         savedInstanceState.putInt(KEY_INDEX,mCurrentIndex);
+        //Saves total number of questions answered
+        savedInstanceState.putInt(TOTAL_ANSWERED,mTotalAnswered);
+        //Saves how many correct answers so far
+        savedInstanceState.putInt(CORRECT_ANSWERS,mCorrectAnswers);
+        //Saves what questions have been answered
+        for(int i = 0;i<mQuestionBank.length;i++){
+            savedInstanceState.putBoolean("q"+i+1,mQuestionBank[i].isQuestionIsAnswered());
+        }
     }
 
     @Override
@@ -50,7 +67,17 @@ public class QuizActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quiz);
         if(savedInstanceState != null){
+            //Loads current question index
             mCurrentIndex = savedInstanceState.getInt(KEY_INDEX,0);
+            //Loads how many correct answers
+            mCorrectAnswers = savedInstanceState.getInt(CORRECT_ANSWERS,0);
+            //Loads total number of questions answered
+            mTotalAnswered = savedInstanceState.getInt(TOTAL_ANSWERED,0);
+            //Loads what questions have been answered or not
+            for(int i = 0;i<mQuestionBank.length;i++){
+             mQuestionBank[i].setQuestionIsAnswered(savedInstanceState.getBoolean("q"+i+1));
+            }
+
         }
         mTrueButton = (Button) findViewById(R.id.true_button);
         mTrueButton.setOnClickListener(new View.OnClickListener() {
@@ -69,6 +96,8 @@ public class QuizActivity extends AppCompatActivity {
         });
 
         mQuestionText = (TextView)findViewById(R.id.question_text_view);
+
+        mQuestAns =(TextView)findViewById(R.id.tvQuestAns);
 
         int question = mQuestionBank[mCurrentIndex].getQuestionResId();
 
@@ -110,17 +139,22 @@ public class QuizActivity extends AppCompatActivity {
 
     /**
      * Updates TextView with question stored in mQuestionBank array
+     * If question has been answered once, True and False button are hidden and text
+     * saying question has been answered shows up
      */
     private void updateQuestion() {
         int question = mQuestionBank[mCurrentIndex].getQuestionResId();
         mQuestionText.setText(question);
         if(mQuestionBank[mCurrentIndex].isQuestionIsAnswered()){
-            mFalseButton.setClickable(false);
-            mTrueButton.setClickable(false);
+            mFalseButton.setVisibility(View.GONE);
+            mTrueButton.setVisibility(View.GONE);
+            mQuestAns.setVisibility(View.VISIBLE);
         }
         else{
-            mFalseButton.setClickable(true);
-            mTrueButton.setClickable(true);
+            mFalseButton.setVisibility(View.VISIBLE);
+            mTrueButton.setVisibility(View.VISIBLE);
+            mQuestAns.setVisibility(View.GONE);
+
         }
     }
 
@@ -135,6 +169,7 @@ public class QuizActivity extends AppCompatActivity {
         int messageResId = 0;
         if(userPressedTrue == answerIsTrue){
             messageResId = R.string.correct_toast;
+            mCorrectAnswers++;
         }
         else{
             messageResId = R.string.incorrect_toast;
@@ -148,9 +183,19 @@ public class QuizActivity extends AppCompatActivity {
         toast.show();
 
         mQuestionBank[mCurrentIndex].setQuestionIsAnswered(true);
+        //Hides True and False button after question was answered. Displays message showing question was answered
+        mFalseButton.setVisibility(View.GONE);
+        mTrueButton.setVisibility(View.GONE);
+        mQuestAns.setVisibility(View.VISIBLE);
+        mTotalAnswered++;
+        float grade = ((float)mCorrectAnswers/(float)mQuestionBank.length)*100;
 
-        mFalseButton.setClickable(false);
-        mTrueButton.setClickable(false);
+        //If last question was answered displays the grade for the quiz
+        if(mTotalAnswered == mQuestionBank.length){
+            toast = Toast.makeText(this,"Your grade:"+grade+"%",Toast.LENGTH_SHORT);
+            toast.setGravity(Gravity.TOP,0,200);
+            toast.show();
+        }
     }
 
 }
